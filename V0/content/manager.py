9abee105d415ee72dcf6d49ac3bb43fb2147ef1b
Jay Reddy <jay.w.reddy@gmail.com>
2013-10-21 23:16:39 -0700

    more processing code

---
+++ V0/content/manager.py
@@ -0,0 +1,10 @@
+from stream import articleGrabber
+
+#if raw folder is empty, run articlegrabber
+
+
+#if clean folder is empty, run cleaner. 
+
+
+#for stock in stocks, grab and clean stock data,
+#then select the sentiments that have it in its entities, compare
